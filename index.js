eeb14b72f3b6eb61f42a4a171dd0ad0b3a43a41f
Nathan Sculli <scull7@gmail.com>
2016-07-21 22:55:19 -0700

    basic functionality.

--- index.js
+++ index.js
@@ -1,52 +1,61 @@
 
 const _         = require('ramda');
+const Bluebird  = require('bluebird');
 const awesomize = require('./lib/awesomize');
 
-const throwMiddlewareNotFunction = (index) => {
-  throw TypeError(`All middleware given must be functions - index: ${index}`);
-};
 
 const throwMiddlewareEmpty = () => {
-  throw TypeError('You must provide at least one function');
+  throw TypeError('You must provide at least one middleware');
 };
 
-const testMiddlewareArray = _.curry((index, mw) => {
-  try {
-    return testMiddleware(mw);
+const defaultSpec = _.always(_.compose(
+  Bluebird.resolve
+, _.prop('data')
+))
 
-  } catch (e) {
-    const result = /((\d:?)+)/.exec(e.message);
 
-    if (_.isNil(result)) {
-      throw new ReferenceError(`Unexpected Error: ${e.message}`);
-    }
+const getSpec = (req) => _.compose(
+  _.ifElse(_.is(Function), awesomize(req), defaultSpec)
+, _.prop('awesomize')
+);
 
-    const parent_index = result[1];
+const getIO = _.propOr((req, data) => data, 'io');
 
-    throwMiddlewareNotFunction(index + ':' + parent_index);
-  }
-});
+
+const getTransform = _.propOr((req, data) => data, 'transform');
 
 
 const run = _.curry((req, middleware) => {
-  const current = _.head(middleware);
-  const spec    = awesomize(req, current.awesomize);
+  const current   = _.head(middleware);
+  const spec      = getSpec(req)(current);
+  const io        = getIO(current);
+  const transform = getTransform(current);
+  const cur_data  = _.propOr({}, 'data', req);
 
   return spec(req)
+
+  .then((data) => Bluebird.props(_.merge(cur_data, io(req, data) ))) 
+
+  .then(transform.bind(current, req))
+
+  .then(_.compose(_.merge(req), _.objOf('data')))
+
+  .then((new_req) => {
+    if (_.length(middleware) > 1) {
+      return run(new_req, _.tail(middleware));
+    }
+
+    return new_req.data;
+  })
+
 });
 
-
-const testMiddleware = (middleware) => middleware.forEach((mw, index) => {
-  return _.cond([
-    [ _.is(Function),   _.always(null) ]
-  , [ Array.isArray,    testMiddlewareArray(index) ]
-  , [ _.T,              () => throwMiddlewareNotFunction(index) ]
-  ])(mw);
-});
 
 const Middleware = (...middleware) => {
   if(middleware.length < 1) throwMiddlewareEmpty();
 
+  //@TODO validate all the middlewar objects.
+  //look in lib/validate.js for inspiration.
 
   return (req, res, next) => {
 
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,47 +1,145 @@
 /*eslint-env node, mocha */
+const _ = require('ramda');
 const expect = require('chai').expect;
 
-const MW     = require('../index.js');
+const JW     = require('../index.js');
+
+
+const basic_middleware = {
+
+  awesomize: (v) => ({
+    foo: {
+      validate: [ v.required ]
+    }
+  })
+
+, io: (req, data) => ({ bar: data.foo })
+
+, transform: (req, data) => data
+};
+
+
+const fail_res = {
+  json: (x) => { throw Error(`Unexpected: ${x}`); }
+}
 
 describe('jigawatt/index.js', () => {
 
-  it('should be a function', () => expect(MW).to.be.a('function'));
+  it('should be a function', () => expect(JW).to.be.a('function'));
 
   
   it('should throw an error when not given any arguments', () => {
 
-    const test = () => MW();
+    const test = () => JW();
 
-    expect(test).to.throw(TypeError, /You must provide at least one function/);
+    expect(test).to.throw(
+      TypeError
+    , /You must provide at least one middleware/
+    );
 
   });
 
 
   it('should call the validation function of the given middleware using ' +
-  'the awesomize module', () => {
-
-    const test_middleware = {
-      awesomize: (v) => ({
-        foo: {
-          validate: [ v.required ]
-        }
-      })
-    };
+  'the awesomize module', (done) => {
 
     const req = {};
 
-    const res = {
-      json: (x) => { throw Error("Unexpected: " + x); }
-    };
+    const test = JW(basic_middleware);
 
-    const test = MW(test_middleware);
-
-    test(req, res, (err) => {
+    test(req, fail_res, (err) => {
       expect(err).to.not.be.undefined;
       expect(err.name).to.eql('ValidationError');
       expect(err.validation.foo).to.eql('required');
+      done();
+    });
+
+  });
+
+  it('should pass the output of the awesomizer as `data` to the IO ' +
+  'function', (done) => {
+
+    const req  = { foo: 'bar' };
+    const mw   = _.merge(basic_middleware, {
+      io: (req, data) => {
+        expect(data.foo).to.eql('bar');
+
+        return data;
+      }
 
     });
+    const test = JW(mw);
+
+    const res  = {
+      json: (data) => {
+        expect(data.foo).to.eql('bar');
+        done();
+
+      }
+    };
+
+    test(req, res, done);
+
+  });
+
+
+  it('should pass the new request object to the second middleware', (done) => {
+
+    const req = { foo: 'bar' };
+    const mw1 = _.merge(basic_middleware, {
+      io: (req, data) => {
+        expect(data.foo).to.eql('bar');
+
+        return { bar: 'baz' };
+      }
+    });
+    const mw2 = _.merge(basic_middleware, {
+      awesomize: (v) => ({
+        bar: {
+          read: _.path([ 'data', 'bar' ])
+        , validate: [ v.required ]
+        }
+      })
+    , io: (req, data) => ({ boo: data.bar })
+    });
+
+    const test = JW(mw1, mw2);
+
+    const res = {
+      json: (data) => {
+
+        expect(data.bar).to.eql('baz');
+        expect(data.boo).to.eql('baz');
+        done();
+
+      }
+    };
+
+    test(req, res, done);
+
+  });
+
+
+  it('should pass in the req.data object if no awesomize function is ' +
+  'provided', (done) => {
+    const req = { foo: 'bar', data: { bar: 'blah' }};
+    const mw1 = {
+      io: (req, data) => {
+        expect(data.bar).to.eql('blah');
+        return data;
+      }
+    };
+
+    const test = JW(mw1);
+
+    const res = {
+      json: (data) => {
+        expect(data.bar).to.eql('blah');
+        done();
+      }
+    };
+
+    test(req, res, done);
 
   });
 
@@ -51,7 +149,7 @@ describe('jigawatt/index.js', () => {
 
     const f1   = () => null;
     const f2   = () => null;
-    const test = () => MW(f1, 'foo', f2);
+    const test = () => JW(f1, 'foo', f2);
     const message = 'All middleware given must be functions - index: 1'
 
     expect(test).to.throw(TypeError, message);
@@ -63,7 +161,7 @@ describe('jigawatt/index.js', () => {
     const f1 = () => null;
     const f2 = () => null;
     const f3 = () => null;
-    const test = () => MW(f1, [f2, 'foo'], f3);
+    const test = () => JW(f1, [f2, 'foo'], f3);
     const message = 'All middleware given must be functions - index: 1:1'
 
     expect(test).to.throw(TypeError, message);
@@ -77,7 +175,7 @@ describe('jigawatt/index.js', () => {
     const f3 = () => null;
     const f4 = () => null;
     const f5 = () => null;
-    const test = () => MW([f2, [f4, f5, 'foo']], f1, f3);
+    const test = () => JW([f2, [f4, f5, 'foo']], f1, f3);
     const message = 'All middleware given must be functions - index: 0:1:2'
 
     expect(test).to.throw(TypeError, message);
@@ -87,7 +185,7 @@ describe('jigawatt/index.js', () => {
   it('should return a function with an arity of 3 when given one argument ' +
   'that is a function', () => {
 
-    const actual = MW(() => null);
+    const actual = JW(() => null);
     expect(actual).to.be.a('function');
     expect(actual.length).to.eql(3);
 
